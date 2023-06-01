7202017f93959354fffa23b0192217ef9c2bb2e4
Beslan <xauxaev@mail.ru>
2023-06-01 20:28:09 +0300

    completed lesson 23 cycles in a cycle

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function(){
-        console.log("Hello");
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+let numberOfFilms = prompt('Сколько фильмов вы посмотрели?')
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+for(let i = 0; i < 2; i++){
+let questionMovies = prompt('Один из последних просмотренных фильмов?'),
+    questionRateMovie = prompt('На сколько оцените его?');
+
+    if(questionMovies != null && questionRateMovie != null
+     && questionMovies != '' && questionRateMovie != null){
+        personalMovieDB.movies[questionMovies] = questionRateMovie;
+       console.log('выполнено');
+        }else{
+            console.log('ошибка');
+            i--;
+
+        }
     }
-};
 
-const john = {
-    health: 100
-};
+    if(personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов");
 
-let a = Object.setPrototypeOf(john, soldier)
-john.sayHello();
+    }else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+        console.log("Вы классический зритель");
+        
+    }else{console.log("Вы киноман");}
+
+console.log(personalMovieDB);
